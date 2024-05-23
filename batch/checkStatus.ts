8fc51b82cffa86b99b7f6486d7f41fec28437600
hirasawa <hirokazu@j-yado.com>
2024-05-23 14:22:18 +0900

    v3 APIに対応

--- batch/checkStatus.ts
+++ batch/checkStatus.ts
@@ -3,19 +3,52 @@ import axios, { AxiosResponse } from 'axios'
 import 'dotenv/config'
 import * as fs from 'fs'
 import { getBlockRecover, webhook } from '../modules/slack'
+import * as process from 'node:process'
 
 /**
  * Identity
  * tokens
  */
 const getTokens = async (): Promise<AxiosResponse> => {
-    const url = 'https://identity.tyo1.conoha.io/v2.0/tokens'
-    return await axios.post(url, {
-        auth: {
-            passwordCredentials: { username: process.env.API_USERNAME, password: process.env.API_PASSWORD },
-            tenantId: process.env.TENANT_ID
-        }
-    })
+    let url = ''
+    switch (process.env.API_VERSION) {
+        case 'v3':
+            url = process.env.API_IDENTITY_SERVICE + '/auth/tokens'
+            return await axios.post(url, {
+                auth: {
+                    identity: {
+                        methods: ['password'],
+                        password: {
+                            user: {
+                                id: process.env.API_USERID,
+                                password: process.env.API_PASSWORD
+                            }
+                        }
+                    },
+                    scope: {
+                        project: {
+                            id: process.env.TENANT_ID
+                        }
+                    }
+                }
+            })
+        default: // v2
+            url = process.env.API_IDENTITY_SERVICE + '/tokens'
+            return await axios.post(url, {
+                auth: {
+                    passwordCredentials: { username: process.env.API_USERNAME, password: process.env.API_PASSWORD },
+                    tenantId: process.env.TENANT_ID
+                }
+            })
+    }
+}
+const parseTokens = async (responseTokens: AxiosResponse) => {
+    switch (process.env.API_VERSION) {
+        case 'v3':
+            return responseTokens.headers['x-subject-token']
+        default: // v2
+            return String(responseTokens.data.access.token.id)
+    }
 }
 
 /**
@@ -23,12 +56,23 @@ const getTokens = async (): Promise<AxiosResponse> => {
  * servers
  */
 const getServers = async (token: string): Promise<AxiosResponse> => {
-    const url: string = 'https://compute.tyo1.conoha.io/v2/' + process.env.TENANT_ID + '/servers'
-    return await axios.get(url, {
-        headers: {
-            'X-Auth-Token': token
-        }
-    })
+    let url = ''
+    switch (process.env.API_VERSION) {
+        case 'v3':
+            url = process.env.API_COMPUTE_SERVICE + '/servers'
+            return await axios.get(url, {
+                headers: {
+                    'X-Auth-Token': token
+                }
+            })
+        default: // v2
+            url = process.env.API_COMPUTE_SERVICE + '/' + process.env.TENANT_ID + '/servers'
+            return await axios.get(url, {
+                headers: {
+                    'X-Auth-Token': token
+                }
+            })
+    }
 }
 
 /**
@@ -36,25 +80,37 @@ const getServers = async (token: string): Promise<AxiosResponse> => {
  * servers/{server_id}
  */
 const getStatus = async (token: string, serverId: string): Promise<AxiosResponse> => {
-    const url: string = 'https://compute.tyo1.conoha.io/v2/' + process.env.TENANT_ID + '/servers/' + serverId
-    return await axios.get(url, {
-        headers: {
-            'X-Auth-Token': token
-        },
-        params: {
-            auth: {
-                passwordCredentials: { username: process.env.API_USERNAME, password: process.env.API_PASSWORD },
-                tenantId: process.env.TENANT_ID
-            }
-        }
-    })
+    let url = ''
+    switch (process.env.API_VERSION) {
+        case 'v3':
+            url = process.env.API_COMPUTE_SERVICE + '/servers/' + serverId
+            return await axios.get(url, {
+                headers: {
+                    'X-Auth-Token': token
+                }
+            })
+        default: // v2
+            url = process.env.API_COMPUTE_SERVICE + '/' + process.env.TENANT_ID + '/servers/' + serverId
+            return await axios.get(url, {
+                headers: {
+                    'X-Auth-Token': token
+                },
+                params: {
+                    auth: {
+                        passwordCredentials: { username: process.env.API_USERNAME, password: process.env.API_PASSWORD },
+                        tenantId: process.env.TENANT_ID
+                    }
+                }
+            })
+    }
 }
 
 ;(async () => {
     try {
-        // トークン取得
+        // トークン情報取得
         const responseTokens: AxiosResponse = await getTokens()
-        const token = String(responseTokens.data.access.token.id)
+        // トークン取得
+        const token = await parseTokens(responseTokens)
 
         // サーバー一覧取得
         const responseServers: AxiosResponse = await getServers(token)
